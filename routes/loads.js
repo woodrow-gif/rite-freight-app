d3e449ff7d9eff7a8de143611eaea519de7e43f2
woodrow-gif <woodrow@ritefreight.net>
2026-03-10 18:47:21 -0400

    Set up routes for loading operations

--- routes/loads.js
+++ routes/loads.js
@@ -1,1 +1,12 @@
+const express = require("express")
+const router = express.Router()
 
+const {
+  getLoads,
+  createLoad
+} = require("../controllers/loadsController")
+
+router.get("/", getLoads)
+router.post("/", createLoad)
+
+module.exports = router
